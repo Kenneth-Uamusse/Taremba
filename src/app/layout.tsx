e0c12d2a1e58ccd62d8d4aec9d3604bb3b70d4fa
Kenneth Uamusse <kennethuamusse10@gmail.com>
2025-09-15 19:17:23 +0200

    First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} `}>{children}</body>
+    </html>
+  );
+}
